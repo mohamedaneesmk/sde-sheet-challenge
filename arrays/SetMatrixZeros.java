0f8e1f2bb1b0abf44b19f6835b5b448f95b4d7d3
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-06-01 11:23:30 +0530

    Set Matrix Zeroes | LeetCode #73 -> Better

--- arrays/SetMatrixZeros.java
+++ arrays/SetMatrixZeros.java
@@ -1,5 +1,7 @@
 package arrays;
 
+/*
+    BRUTE FORCEC SOLUTION
 public class SetMatrixZeros {
     public static void main(String[] args) {
         int[][] matrix = {
@@ -56,6 +58,52 @@ public class SetMatrixZeros {
             if (matrix[row][col] != 0) {
                 matrix[row][col] = -1;
             }
+        }
+    }
+}
+*/
+
+// BETTER SOLUTION
+public class SetMatrixZeros {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                { 1, 1, 1 },
+                { 1, 0, 1 },
+                { 1, 1, 1 }
+        };
+
+        setMatrixZeros(matrix);
+        printMatrix(matrix);
+    }
+
+    private static void setMatrixZeros(int[][] matrix) {
+        int[] rowMatrix = new int[matrix.length];
+        int[] colMatrix = new int[matrix[0].length];
+
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[0].length; col++) {
+                if (matrix[row][col] == 0) {
+                    rowMatrix[row] = 1;
+                    colMatrix[col] = 1;
+                }
+            }
+        }
+
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[0].length; col++) {
+                if (rowMatrix[row] == 1 || colMatrix[col] == 1) {
+                    matrix[row][col] = 0;
+                }
+            }
+        }
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[0].length; col++) {
+                System.out.print(matrix[row][col] + " ");
+            }
+            System.out.println();
         }
     }
 }
