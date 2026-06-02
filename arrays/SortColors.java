50b25e926cca82d3a18cb4d17a4c05ddde847dd0
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-06-02 18:48:36 +0530

    Sort Colors | LeetCode #75 -> Better

--- arrays/SortColors.java
+++ arrays/SortColors.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-// BRUTE FORCE SOLUTION
+// BETTER FORCE SOLUTION
 public class SortColors {
     public static void main(String[] args) {
         int[] nums = { 2, 0, 2, 1, 1, 0 };
