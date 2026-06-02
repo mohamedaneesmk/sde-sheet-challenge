1422989d8d722bb85e347f3df57f4cba13776c6f
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-06-02 17:28:06 +0530

    Next Permutation | LeetCode #31 -> Optimal

--- arrays/NextPermutation.java
+++ arrays/NextPermutation.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class NextPermutation {
     public static void main(String[] args) {
         int[] nums = { 1, 2, 3 };
-        nextPermutation(nums);
+        nextPermutation(nums); 
 
         System.out.println(Arrays.toString(nums));
     }
