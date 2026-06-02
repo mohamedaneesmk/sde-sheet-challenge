dcc8861c8b606f84782a1be1d37d13c6dc471af3
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-06-02 17:55:20 +0530

    Maximum SubArray | LeetCode #53 -> Brute force

--- arrays/MaximumSubarray.java
+++ arrays/MaximumSubarray.java
@@ -9,7 +9,7 @@ public class MaximumSubarray {
         int maxSum = Integer.MIN_VALUE;
 
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i; j < nums.length; j++) {
+            for (int j = i; j < nums.length; j++) { 
                 int sum = 0;
                 for (int k = i; k < j; k++) {
                     sum = sum + nums[k];
