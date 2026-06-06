3cf262c37df11b4a50ed89b0aecc9f6cd68c5f3d
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-06-06 18:16:43 +0530

    Majority Element II | LeetCode #229 -> Boyer-Moore Voting Algorithm

---
+++ arrays/MajorityElementII.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class MajorityElementII {
+    public static void main(String[] args) {
+        int[] nums = { 1,2 };
+        List<Integer> result = majorityElementII(nums);
+        System.out.println(result);
+    }
+
+    private static List<Integer> majorityElementII(int[] nums) {
+        int candidate1 = 0, candidate2 = 0;
+        int count1 = 0, count2 = 0;
+
+        for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            } else if (count1 == 0) {
+                candidate1 = num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+
+        count1 = 0;
+        count2 = 0;
+
+        for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            }
+        }
+
+        List<Integer> result = new ArrayList<>();
+
+        if (count1 > nums.length / 3) {
+            result.add(candidate1);
+        }
+
+        if (count2 > nums.length / 3) {
+            result.add(candidate2);
+        }
+
+        return result;
+    }
+}
