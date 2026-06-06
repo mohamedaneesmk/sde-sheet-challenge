89d08e19a35eb1d6f5fbef803179b8fa9209d978
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-06-06 23:17:06 +0530

    Find Duplicates | LeetCode #287 -> Optimal

--- arrays/FindDuplicate.java
+++ arrays/FindDuplicate.java
@@ -1,4 +1,7 @@
-import java.util.Arrays;
+// import java.util.Arrays;
+
+// import java.util.HashMap;
+// import java.util.Map;
 
 public class FindDuplicate {
     public static void main(String[] args) {
@@ -6,16 +9,57 @@ public class FindDuplicate {
         System.out.println(findDuplicate(nums));
     }
 
+    // USING LINKED LIST
     private static int findDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] == nums[j]) {
-                    return nums[i];
-                }
-            }
+        int slow = nums[0];
+        int fast = nums[0];
+
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        fast = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
         }
 
-        return -1;
+        return fast;
     }
+
+    /*
+     * HASHMAP SOLUTION
+     * private static int findDuplicate(int[] nums) {
+     * Map<Integer,Integer> map = new HashMap<>();
+     * 
+     * for(int num : nums){
+     * map.put(num, map.getOrDefault(num, 0) + 1);
+     * }
+     * 
+     * for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+     * if (entry.getValue() > 1) {
+     * return entry.getKey();
+     * }
+     * }
+     * return -1;
+     * }
+     */
+
+    /*
+     * BRUTE FORCE SOLUTION
+     * private static int findDuplicate(int[] nums) {
+     * Arrays.sort(nums);
+     * for (int i = 0; i < nums.length; i++) {
+     * for (int j = i + 1; j < nums.length; j++) {
+     * if (nums[i] == nums[j]) {
+     * return nums[i];
+     * }
+     * }
+     * }
+     * 
+     * return -1;
+     * }
+     */
+
 }
