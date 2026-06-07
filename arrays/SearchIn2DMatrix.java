cbb8a8a3a830b040941b2c932035e79facdfaf5f
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-06-07 22:15:19 +0530

    Search a 2D Matrix | LeetCode #74 -> Optimal

---
+++ arrays/SearchIn2DMatrix.java
@@ -0,0 +1,34 @@
+public class SearchIn2DMatrix {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                { 1, 3, 5, 7 },
+                { 10, 11, 16, 20 },
+                { 23, 30, 34, 60 }
+        };
+
+        System.out.println(isTargetfound(matrix, 35));
+    }
+
+    private static boolean isTargetfound(int[][] matrix, int target) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int left = 0;
+        int right = rows * cols - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int value = matrix[mid / cols][mid % cols];
+
+            if (value == target) {
+                return true;
+            } else if (value < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return false;
+    }
+}
