a44929435a2f15c11d9134649ad1b448d2533feb
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-06-07 22:46:37 +0530

    Merge Sorted Array | LeetCode #88 -> Optimal

--- arrays/MergeSortedArray.java
+++ arrays/MergeSortedArray.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class MergeSortedArray {
     public static void main(String[] args) {
         int[] nums1 = { 1, 2, 3, 0, 0, 0 };
@@ -6,6 +8,7 @@ public class MergeSortedArray {
         int n = 3;
 
         merge(nums1, m, nums2, n);
+        System.out.println(Arrays.toString(nums1));
     }
 
     private static void merge(int[] nums1, int m, int[] nums2, int n) {
