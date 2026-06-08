6ad43b5e3e0a46a4f11d9d2666b49db7c68139da
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-06-08 12:35:12 +0530

    Longest Substring Without Repeating Characters | LeetCode #3 -> Optimal

---
+++ arrays/LongestUniqueSubstring.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+
+public class LongestUniqueSubstring {
+    public static void main(String[] args) {
+        String string = "abcabcbb";
+        System.out.println(findLongestUniqueSubstring(string));
+    }
+
+    private static int findLongestUniqueSubstring(String string) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        int left = 0;
+        int maxLength = 0;
+
+        for (int right = 0; right < string.length(); right++) {
+            char ch = string.charAt(right);
+
+            if (map.containsKey(ch)) {
+                left = Math.max(left, map.get(ch) + 1);
+            }
+
+            map.put(ch, right);
+            int windowSize = right - left + 1;
+            maxLength = Math.max(maxLength, windowSize);
+        }
+
+        return maxLength;
+    }
+}
