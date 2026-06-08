c29caa8b27b40f0a2116c61de51948755a698bb8
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-06-08 12:52:31 +0530

    Longest Subarray with Sum K | GeeksforGeeks -> Optimal

--- arrays/LongestSubarray.java
+++ arrays/LongestSubarray.java
@@ -28,7 +28,7 @@
 */
 
 // BETTER SOLUTION
-public class LongestSubarray {
+/*public class LongestSubarray {
     public static void main(String[] args) {
         int[] arr = { 10, 5, 2, 7, 1, -10 };
         int k = 15;
@@ -45,6 +45,43 @@ public class LongestSubarray {
                 if (sum == k) {
                     maxLength = Math.max(maxLength, j - i + 1);
                 }
+            }
+        }
+
+        return maxLength;
+    }
+}*/
+
+// OPTIMAL SOLUTION
+
+import java.util.HashMap;
+
+public class LongestSubarray {
+    public static void main(String[] args) {
+        int[] arr = { 10, 5, 2, 7, 1, -10 };
+        int k = 15;
+        System.out.println(longestSubarray(arr, k));
+    }
+
+    private static int longestSubarray(int[] arr, int k) {
+        HashMap<Long, Integer> map = new HashMap<>();
+
+        long sum = 0;
+        int maxLength = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+
+            if (sum == k) {
+                maxLength = i + 1;
+            }
+
+            if (map.containsKey(sum - k)) {
+                maxLength = Math.max(maxLength, i - map.get(sum - k));
+            }
+
+            if (!map.containsKey(sum)) {
+                map.put(sum, i);
             }
         }
 
