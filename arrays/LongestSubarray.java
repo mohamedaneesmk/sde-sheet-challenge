dc4d16714212c53e87d1036fa6c14c0aa967b969
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-06-08 12:43:18 +0530

    Longest Subarray with Sum K | GeeksforGeeks -> Brute force

---
+++ arrays/LongestSubarray.java
@@ -0,0 +1,26 @@
+public class LongestSubarray {
+    public static void main(String[] args) {
+        int[] arr = { 10, 5, 2, 7, 1, -10 };
+        int k = 15;
+        System.out.println(longestSubarray(arr, k));
+    }
+
+    private static int longestSubarray(int[] arr, int k) {
+        int maxLength = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                int sum = 0;
+                for (int x = i; x <= j; x++) {
+                    sum = sum + arr[x];
+                }
+
+                if (sum == k) {
+                    maxLength = Math.max(maxLength, j - i + 1);
+                }
+            }
+        }
+
+        return maxLength;
+    }
+}
