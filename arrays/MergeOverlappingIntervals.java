3ca19a117c416fff7369295d419d96476006dabf
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-06-10 20:35:38 +0530

    Merge Overlapping Intervals | LeetCode #56 -> Optimal

---
+++ arrays/MergeOverlappingIntervals.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MergeOverlappingIntervals {
+    public static void main(String[] args) {
+        int[][] intervals = { { 1, 3 }, { 2, 6 }, { 8, 10 }, { 15, 18 } };
+        int[][] result = mergeOverlappingIntervals(intervals);
+
+        System.out.println(Arrays.deepToString(result));
+    }
+
+    private static int[][] mergeOverlappingIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+        List<int[]> answer = new ArrayList<>();
+
+        for (int[] interval : intervals) {
+            if (answer.isEmpty() || answer.get(answer.size() - 1)[1] < interval[0]) {
+                answer.add(interval);
+            } else {
+                answer.get(answer.size() - 1)[1] = Math.max(answer.get(answer.size() - 1)[1], interval[1]);
+            }
+        }
+
+        return answer.toArray(new int[answer.size()][]);
+    }
+}
