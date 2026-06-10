67ab7f2301dfdf02a020bef0708a1432f1bb7616
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-06-10 21:50:30 +0530

    Inversion Of Array | GeeksforGeeks -> Better

--- arrays/InversionOfArray.java
+++ arrays/InversionOfArray.java
@@ -1,3 +1,28 @@
+/* BRUTE FORCE
+ * public class InversionOfArray {
+ * public static void main(String[] args) {
+ * int[] arr = { 4, 3, 2, 1 };
+ * System.out.println(inversionOfArray(arr));
+ * }
+ * 
+ * private static int inversionOfArray(int[] arr) {
+ * int len = arr.length;
+ * int count = 0;
+ * 
+ * for (int i = 0; i < len - 1; i++) {
+ * for (int j = i + 1; j < len; j++) {
+ * if (arr[i] > arr[j]) {
+ * count++;
+ * }
+ * }
+ * }
+ * 
+ * return count;
+ * }
+ * }
+ */
+
+// BETTER SOLUTION
 public class InversionOfArray {
     public static void main(String[] args) {
         int[] arr = { 4, 3, 2, 1 };
@@ -5,15 +30,17 @@ public class InversionOfArray {
     }
 
     private static int inversionOfArray(int[] arr) {
-        int len = arr.length;
         int count = 0;
 
-        for (int i = 0; i < len - 1; i++) {
-            for (int j = i + 1; j < len; j++) {
+        for (int i = 0; i < arr.length; i++) {
+            int smaller = 0;
+            for (int j = i + 1; j < arr.length; j++) {
                 if (arr[i] > arr[j]) {
-                    count++;
+                    smaller++;
                 }
             }
+
+            count += smaller;
         }
 
         return count;
