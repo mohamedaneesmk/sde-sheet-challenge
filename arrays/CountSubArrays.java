aa47a610233ac45c98294acad9fcb2c2796409c6
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-06-15 11:20:41 +0530

    Count Subarrays with given XOR | Geeks for Geeks  -> Better

--- arrays/CountSubArrays.java
+++ arrays/CountSubArrays.java
@@ -1,3 +1,34 @@
+// Count Subarrays with given XOR - Brute Force
+/*
+ * public class CountSubArrays {
+ * public static void main(String[] args) {
+ * int[] arr = { 4, 2, 2, 6, 4 };
+ * int k = 6;
+ * 
+ * System.out.println(countSubArrays(arr, k));
+ * }
+ * 
+ * private static int countSubArrays(int[] arr, int k) {
+ * int count = 0;
+ * 
+ * for (int i = 0; i < arr.length; i++) {
+ * for (int j = i; j < arr.length; j++) {
+ * int xor = 0;
+ * for (int x = i; x <= j; x++) {
+ * xor = xor ^ arr[x];
+ * }
+ * if (xor == k) {
+ * count++;
+ * }
+ * }
+ * }
+ * 
+ * return count;
+ * }
+ * }
+ */
+
+// BETTER SOLUTION
 public class CountSubArrays {
     public static void main(String[] args) {
         int[] arr = { 4, 2, 2, 6, 4 };
@@ -10,11 +41,9 @@ public class CountSubArrays {
         int count = 0;
 
         for (int i = 0; i < arr.length; i++) {
+            int xor = 0;
             for (int j = i; j < arr.length; j++) {
-                int xor = 0;
-                for (int x = i; x <= j; x++) {
-                    xor = xor ^ arr[x];
-                }
+                xor = xor ^ arr[j];
                 if (xor == k) {
                     count++;
                 }
