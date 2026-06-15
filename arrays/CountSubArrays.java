6257da4ee61b9c3c56bcbc4e93926822359555c4
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-06-15 11:34:27 +0530

    Count Subarrays with given XOR | Geeks for Geeks  -> Better

--- arrays/CountSubArrays.java
+++ arrays/CountSubArrays.java
@@ -29,6 +29,36 @@
  */
 
 // BETTER SOLUTION
+/*
+ * public class CountSubArrays {
+ * public static void main(String[] args) {
+ * int[] arr = { 4, 2, 2, 6, 4 };
+ * int k = 6;
+ * 
+ * System.out.println(countSubArrays(arr, k));
+ * }
+ * 
+ * private static int countSubArrays(int[] arr, int k) {
+ * int count = 0;
+ * 
+ * for (int i = 0; i < arr.length; i++) {
+ * int xor = 0;
+ * for (int j = i; j < arr.length; j++) {
+ * xor = xor ^ arr[j];
+ * if (xor == k) {
+ * count++;
+ * }
+ * }
+ * }
+ * 
+ * return count;
+ * }
+ * }
+ */
+
+// Optimal
+import java.util.HashMap;
+
 public class CountSubArrays {
     public static void main(String[] args) {
         int[] arr = { 4, 2, 2, 6, 4 };
@@ -37,17 +67,23 @@ public class CountSubArrays {
         System.out.println(countSubArrays(arr, k));
     }
 
-    private static int countSubArrays(int[] arr, int k) {
-        int count = 0;
+    private static long countSubArrays(int[] arr, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
 
-        for (int i = 0; i < arr.length; i++) {
-            int xor = 0;
-            for (int j = i; j < arr.length; j++) {
-                xor = xor ^ arr[j];
-                if (xor == k) {
-                    count++;
-                }
-            }
+        int xor = 0;
+        long count = 0;
+
+        map.put(0, 1);
+
+        for (int num : arr) {
+
+            xor = xor ^ num;
+
+            int target = xor ^ k;
+
+            count = count + map.getOrDefault(target, 0) + 1;
+
+            map.put(xor, map.getOrDefault(xor, 0) + 1);
         }
 
         return count;
