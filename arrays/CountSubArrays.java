5e80143fcbcd15798b57483a83d7e95ae71ab97b
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-06-15 11:13:57 +0530

    Count Subarrays with given XOR | Geeks for Geeks  -> Brute Force

---
+++ arrays/CountSubArrays.java
@@ -0,0 +1,26 @@
+public class CountSubArrays {
+    public static void main(String[] args) {
+        int[] arr = { 4, 2, 2, 6, 4 };
+        int k = 6;
+
+        System.out.println(countSubArrays(arr, k));
+    }
+
+    private static int countSubArrays(int[] arr, int k) {
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                int xor = 0;
+                for (int x = i; x <= j; x++) {
+                    xor = xor ^ arr[x];
+                }
+                if (xor == k) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
